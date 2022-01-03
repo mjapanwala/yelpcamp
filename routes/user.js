250151d8688ad35f0aac26f1e0357f19041a71b1
mjapanwala <55627647+mjapanwala@users.noreply.github.com>
2022-01-03 17:07:01 -0500

    using Router

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,24 @@
-const { Router } = require('express')
+const express = require("express")
+const router = express.Router()
 
 
 
-
-Router.get('/login', (req, res) => {
-    
+router.get('/', (req, res) => {
+    res.send("Hello this is the login  page")
 })
+router.get('/two', (req, res) => {
+    res.send("Hello this is the two login page")
+})
+router.get('/three', (req, res) => {
+    res.send("Hello this is the three login page")
+})
+router.get('/four', (req, res) => {
+    res.send("Hello this is the four login page")
+})
+
+router.post('/admins', (req, res) => {
+    res.send("You have gotten into the admin page")
+})
+
+
+module.exports = router;
